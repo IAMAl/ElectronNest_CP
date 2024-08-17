d0c103362537eed415bc65487291bd2d45b8e254
takano <rentsrule@gmail.com>
2024-08-17 18:45:33 +0900

    first commit

---
+++ asm/test.asm
@@ -0,0 +1,128 @@
+MyID:0
+TrueID:0
+FalseID:0
+zldb n #15 #1 #5 m
+mv e
+cnct #4 #9
+
+MyID:0
+TrueID:0
+FalseID:0
+mv n
+zldb l #256 #1 #512 m
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+cnct #4 #9
+
+MyID:0
+TrueID:0
+FalseID:0
+mv n
+zldb l #256 #1 #512 m
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+cnct #4 #9
+
+MyID:0
+TrueID:0
+FalseID:0
+mv n
+zldb l #256 #1 #512 m
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+cnct #4 #9
+
+MyID:0
+TrueID:0
+FalseID:0
+mv n
+zldb l #256 #1 #512 m
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+cnct #5 #4
+
+MyID:0
+TrueID:0
+FalseID:0
+mv n
+mv src1
+mv e
+zrstb n #256 #1 #0 o
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+zbldb n #256 #1 #128 o
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+cnct #5 #4
+
+MyID:0
+TrueID:0
+FalseID:0
+mv n
+mv src1
+mv e
+zrstb n #256 #1 #0 o
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+zbldb n #256 #1 #128 o
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+cnct #5 #4
+
+MyID:0
+TrueID:0
+FalseID:0
+mv n
+mv src1
+mv e
+zrstb n #256 #1 #0 o
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+zbldb n #256 #1 #128 o
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+cnct #5 #4
+
+MyID:0
+TrueID:0
+FalseID:0
+mv n
+mv src1
+mv e
+zrstb n #256 #1 #0 o
+
+MyID:0
+TrueID:0
+FalseID:0
+mv e
+zbldb n #256 #1 #128 o
