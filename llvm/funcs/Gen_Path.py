7d9dfed20de2d9d07240f9a03d36b657efc552ff
takano <rentsrule@gmail.com>
2024-09-22 06:33:08 +0900

    backup

--- llvm/funcs/Gen_Path.py
+++ llvm/funcs/Gen_Path.py
@@ -173,211 +173,106 @@ def Explore_Path( am, NodeList, path ):
 
         print(f"NNodes:{NNodes}  mnemonic:{mnemonic}")
 
-        if is_StNode( mnemonic ) and not start_st:
-            # Node is first store instruction
-            start_st = True
-            path.StPush( index )
+        if not is_LeafNode( mnemonic, index ):
 
             # Register first arriving the branch node
             if len(NNodes) >= 2 and index not in Branch:
                 Branch.append( index )
                 print(f"Branch: {Branch}")
 
-            # Register Path Node
-            st_ld_path.append( index )
-            st_route_path.append( index )
-            st_leaf_path.append( index )
-
             CountNodes += 1
             PtrList[ index ] += 1
 
-            tmp_index = index
-            if PtrList[ index ] > len( NNodes ):
-                index = path.Pop()
-                popped = True
-                print(f"Popped at St Node")
-            else:
-                path.Push( index )
-                index = NNodes[ PtrList[ index ] - 1 ]
-                print(f"PtrList[ tmp_index ]:{PtrList[ tmp_index ]}")
+            if is_StNode( mnemonic ) and not start_st:
+                # Node is first store instruction
+                start_st = True
+                path.StPush( index )
 
-            # Set explored node
-            em = SetExplored( em, tmp_index, index)
-
-        elif is_StNode( mnemonic ) and start_st:
-            # Node is store instruction
-            #   support multiple store instructions in a basic block
-            start_st = True
-            path.StPush( index )
-
-            # Register first arriving the branch node
-            if len(NNodes) >= 2 and index not in Branch:
-                Branch.append( index )
-                print(f"Branch: {Branch}")
-
-            # Register Path Node
-            st_ld_path.append( index )
-            st_route_path.append( index )
-            st_leaf_path.append( index )
-
-            CountNodes += 1
-            PtrList[ index ] += 1
-
-            tmp_index = index
-            if PtrList[ index ] > len( NNodes ):
-                index = path.Pop()
-                popped = True
-                print(f"Popped at St Node")
-            else:
-                path.Push( index )
-                index = NNodes[ PtrList[ index ] - 1 ]
-                print(f"PtrList[ tmp_index ]:{PtrList[ tmp_index ]}")
-
-            # Set explored node
-            em = SetExplored( em, tmp_index, index)
-
-        elif is_LdNode( mnemonic ) and not start_ld:
-            # Node is first load instruction
-            start_ld = True
-            path.LdPush( index )
-
-            # Register first arriving the branch node
-            if len(NNodes) > 2 and index not in Branch:
-                Branch.append( index )
-                print(f"Branch: {Branch}")
-
-            # Register Path Node
-            if start_st:
-                st_route_path.append( index )
-                st_leaf_path.append( index )
-                st_ld_path.append( index)
-                print(f"st_route_path: {st_route_path}")
-                print(f"st_leaf_path: {st_leaf_path}")
-                print(f"st_ld_path: {st_ld_path}")
-                path.Register( 'st_ld_path', st_ld_path )
-                st_ld_path = []
-
-            # Register Path Node
-            ld_ld_path.append( index )
-            ld_leaf_path.append( index )
-            print(f"ld_ld_path: {ld_ld_path}")
-            print(f"ld_leaf_path: {ld_leaf_path}")
-
-            CountNodes += 1
-            PtrList[ index ] += 1
-
-            print(f"Check : {PtrList[ index ]} at index:{index}")
-
-            tmp_index = index
-            if PtrList[ index ] > len( NNodes ):
-                index = path.Pop()
-                popped = True
-                print(f"Popped at Ld Node")
-            else:
-                path.Push( index )
-                index = NNodes[ PtrList[ index ] - 1 ]
-
-                if PtrList[ index ] > 0 and index in st_route_path:
-                    index = NNodes[ PtrList[ tmp_index ] ]
-
-            # Set explored node
-            em = SetExplored( em, tmp_index, index)
-
-        elif is_LdNode( mnemonic ) and start_ld:
-            # Node is second load instruction
-            start_ld = False
-
-
-            # Register first arriving the branch node
-            if len(NNodes) > 2 and index not in Branch:
-                Branch.append( index )
-                print(f"Branch: {Branch}")
-
-            if start_st:
-                StPath.append( index )
-                st_ld_path.append( index )
-                st_route_path.append( index )
-                st_leaf_path.append( index )
-                print(f"st_ld_path: {st_ld_path}")
-                print(f"st_route_path: {st_route_path}")
-                print(f"st_leaf_path: {st_leaf_path}")
-                st_ld_path = []
-
-            # Register Path Node
-            ld_ld_path.append( index )
-            ld_leaf_path.append( index )
-            path.Register( 'ld_ld_path', ld_ld_path )
-            print(f"ld_ld_path: {ld_ld_path}")
-            ld_ld_path = []
-
-            CountNodes += 1
-            PtrList[ index ] += 1
-
-            tmp_index = index
-            if PtrList[ index ] > len( NNodes ):
-                index = path.Pop()
-                popped = True
-                print(f"Popped at Ld Node")
-            else:
-                path.Push( index )
-                index = NNodes[ PtrList[ index ] - 1 ]
-
-                if PtrList[ index ] > 0 and index in st_route_path:
-                    index = NNodes[ PtrList[ tmp_index ] ]
-
-            # Set explored node
-            em = SetExplored( em, tmp_index, index)
-
-        elif not is_LeafNode( mnemonic, index ):
-            print(f"This is NOT LEAF Node: {Branch}")
-            if len(NNodes) > 2 and index not in Branch and not BR_Popped:
-                # skip first entry
-                #   first entry is parent node
-                PtrList[ index ] += 2
-            else:
-                PtrList[ index ] += 1
-
-            # Register first arriving the branch node
-            if len(NNodes) > 2 and index not in st_leaf_path:
-                Branch.append( index )
-                print(f"Branch: {Branch}")
-
-            if start_st:
-                StPath.append( index )
+                # Register Path Node
                 st_ld_path.append( index )
                 st_route_path.append( index )
                 st_leaf_path.append( index )
 
-            if start_ld:
-                LdPath.append( index )
+            elif is_StNode( mnemonic ) and start_st:
+                # Node is store instruction
+                #   support multiple store instructions in a basic block
+                start_st = True
+                path.StPush( index )
+
+                # Register Path Node
+                st_ld_path.append( index )
+                st_route_path.append( index )
+                st_leaf_path.append( index )
+
+            elif is_LdNode( mnemonic ) and not start_ld:
+                # Node is first load instruction
+                start_ld = True
+                path.LdPush( index )
+
+                # Register Path Node
+                if start_st:
+                    st_route_path.append( index )
+                    st_leaf_path.append( index )
+                    st_ld_path.append( index)
+                    print(f"st_route_path: {st_route_path}")
+                    print(f"st_leaf_path: {st_leaf_path}")
+                    print(f"st_ld_path: {st_ld_path}")
+                    path.Register( 'st_ld_path', st_ld_path )
+                    st_ld_path = []
+
+                # Register Path Node
                 ld_ld_path.append( index )
                 ld_leaf_path.append( index )
+                print(f"ld_ld_path: {ld_ld_path}")
+                print(f"ld_leaf_path: {ld_leaf_path}")
 
-            CountNodes += 1
-            print(f"Check : {PtrList[ index ]} at index:{index}")
+            elif is_LdNode( mnemonic ) and start_ld:
+                # Node is second load instruction
+                start_ld = False
+
+                if start_st:
+                    StPath.append( index )
+                    st_ld_path.append( index )
+                    st_route_path.append( index )
+                    st_leaf_path.append( index )
+                    print(f"st_ld_path: {st_ld_path}")
+                    print(f"st_route_path: {st_route_path}")
+                    print(f"st_leaf_path: {st_leaf_path}")
+                    st_ld_path = []
+
+                # Register Path Node
+                ld_ld_path.append( index )
+                ld_leaf_path.append( index )
+                path.Register( 'ld_ld_path', ld_ld_path )
+                print(f"ld_ld_path: {ld_ld_path}")
+                ld_ld_path = []
+
+            else:
+                print(f"This is NOT LEAF Node: {Branch}")
+
+                if start_st:
+                    StPath.append( index )
+                    st_ld_path.append( index )
+                    st_route_path.append( index )
+                    st_leaf_path.append( index )
+
+                if start_ld:
+                    LdPath.append( index )
+                    ld_ld_path.append( index )
+                    ld_leaf_path.append( index )
 
             tmp_index = index
-            if PtrList[ index ] > len( NNodes ):
-                popped = True
-                path.Push( index )
-                index = NNodes[ PtrList[ tmp_index ] - 1 ]
-                em = SetExplored( em, tmp_index, index)
+            if PtrList[ index ] >= len( NNodes ):
+                index = Branch.pop(-1)
+                print("Branch Popped")
             else:
-                if BR_Popped and len(NNodes) == 2:
-                    index = NNodes[ PtrList[ index ] ]
-                else:
-                    index = NNodes[ PtrList[ index ] - 1 ]
-
-                if PtrList[ index ] > 0 and index in st_route_path and PtrList[ tmp_index ] < len(NNodes):
-                    index = NNodes[ PtrList[ tmp_index ] ]
-
-                print(f"index : {index}")
+                index = NNodes[ PtrList[ index ] - 1 ]
 
             # Set explored node
             em = SetExplored( em, tmp_index, index)
-            BR_Popped = False
 
         elif is_LeafNode( mnemonic, index ):
+
             print("This is LEAF Node")
             tmp_st_leaf_path = []
             PtrList[ index ] += 1
@@ -393,7 +288,6 @@ def Explore_Path( am, NodeList, path ):
                 path.Register( 'st_leaf_path', st_leaf_path )
                 print(f"st_route_path: {st_route_path}")
                 print(f"st_leaf_path: {st_leaf_path}")
-                tmp_st_leaf_path = st_leaf_path
                 st_ld_path = []
                 st_route_path = []
                 st_leaf_path = []
@@ -408,32 +302,16 @@ def Explore_Path( am, NodeList, path ):
                 ld_leaf_path = []
                 start_ld = False
 
-            CountNodes += 1
-
-            print(f"Check : {PtrList[ index ]} at index:{index}")
-
             # Set explored node
             em = SetExplored( em, tmp_index, index)
-            if PtrList[ index ] > len( NNodes ):
-                index = path.Pop()
-                popped = True
-                print(f"Popped at LEAF Node")
-            else:
-                path.Push( index )
-                tmp_index = index
-                if len(Branch) > 0 and index in tmp_st_leaf_path:
-                    index = Branch.pop(-1)
-                    # ToDo
-                    #index = Branch.pop(-1)
-                    print("Branch Popped")
-                    BR_Popped = True
-                else:
-                    index = NNodes[ PtrList[ index ] - 1 ]
+            path.Push( index )
+            index = Branch.pop(-1)
+            print("Branch Popped")
+
 
         # Check Remained Node
         #   exit when there is not remained
         nlist = GetNonExploredNodes( am, em )
-
         print(f"  nlist:{nlist}")
         if popped:
             popped = False
